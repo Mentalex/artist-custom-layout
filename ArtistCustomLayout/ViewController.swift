aa1a2cf3b671b25ce86627ad9e7853785c1f6f13
Alex Tapia <alejandrovztapia@gmail.com>
2020-06-14 16:38:07 -0500

    init: Added initial project

---
+++ ArtistCustomLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtistCustomLayout
+//
+//  Created by Alex Tapia on 14/06/20.
+//  Copyright © 2020 AlexTapia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
